5ed9e6f05132812802ad5affb72789d0f4af8480
Abstrekt <saammmmmmy@gmail.com>
2020-10-28 21:00:02 +0530

    initial setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,29 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Submissions from '../views/Submissions.vue';
+import Submission from '../views/Submission.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+	{
+		path: '/submissions',
+		name: 'Submissions',
+		component: Submissions,
+	},
+	{
+		path: '/submissions/:id',
+		name: 'About',
+		component: Submission,
+	},
+	{
+		path: '*',
+		redirect: '/submissions',
+	},
+];
 
 const router = new VueRouter({
-  routes
-})
+	routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Submission.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="">
+    <h1>Submission Details</h1>
+  </div>
+</template>
