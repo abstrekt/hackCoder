2ba00821f54e1af480e8debb5d7cf56aa52cd401
Abstrekt <saammmmmmy@gmail.com>
2020-10-29 22:43:27 +0530

    integrated codemirror plugin

---
+++ src/components/Codemirror.vue
@@ -0,0 +1,49 @@
+<template>
+	<!-- Two-way Data-Binding -->
+	<codemirror v-model="code" :options="cmOptions" />
+</template>
+
+<script>
+import { codemirror } from 'vue-codemirror';
+import 'codemirror/lib/codemirror.css';
+
+// import language js
+import 'codemirror/mode/javascript/javascript.js';
+
+// import theme style
+import 'codemirror/theme/base16-dark.css';
+
+// import more 'codemirror/some-resource...'
+
+export default {
+	data() {
+		return {
+			code: 'const a = 10',
+			cmOptions: {
+				tabSize: 4,
+				mode: 'text/javascript',
+				theme: 'base16-dark',
+				lineNumbers: true,
+				line: true,
+				// more CodeMirror options...
+			},
+		};
+	},
+	components: {
+		codemirror,
+	},
+
+	methods: {
+		onCmReady(cm) {
+			console.log('the editor is readied!', cm);
+		},
+		onCmFocus(cm) {
+			console.log('the editor is focused!', cm);
+		},
+		onCmCodeChange(newCode) {
+			console.log('this is new code', newCode);
+			this.code = newCode;
+		},
+	},
+};
+</script>
--- src/views/Submission.vue
+++ src/views/Submission.vue
@@ -1,12 +1,18 @@
 <template>
-	<submissionDetails />
+	<div class="">
+		<submissionDetails />
+		<Codemirror />
+	</div>
 </template>
 
 <script>
 import submissionDetails from '../components/Submission-Card.vue';
+import Codemirror from '../components/Codemirror.vue';
+
 export default {
 	components: {
 		submissionDetails,
+		Codemirror,
 	},
 };
 </script>
