6ce905ffd2b0b81e16473105a0371130ab579220
Abstrekt <saammmmmmy@gmail.com>
2020-10-29 16:01:40 +0530

    -m fake data, -a v-data-table

--- src/views/Submissions.vue
+++ src/views/Submissions.vue
@@ -1,15 +1,45 @@
 <template>
-	<div class="">
-		submissions
-	</div>
+	<v-data-table
+		:headers="headers"
+		:items="submissions"
+		:items-per-page="10"
+	>
+	</v-data-table>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-	name: 'Submissions',
-	components: {},
+	data() {
+		return {
+			headers: [
+				{
+					text: 'Time Submitted',
+					value: 'time',
+					align: 'start',
+				},
+				{
+					text: 'Problem Name',
+					value: 'problem',
+				},
+				{
+					text: 'Result',
+					value: 'result',
+				},
+				{
+					text: 'Time Taken',
+					value: 'runtime',
+				},
+				{
+					text: 'Memory Taken',
+					value: 'memory',
+				},
+				{
+					text: 'Language',
+					value: 'language',
+				},
+			],
+			submissions: this.$store.state.all_submissions,
+		};
+	},
 };
 </script>
