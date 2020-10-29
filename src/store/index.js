3f9ff66dfe7ae29454cd06970dd932434f04e4be
Abstrekt <saammmmmmy@gmail.com>
2020-10-29 15:28:24 +0530

    generated fake data

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,88 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+const state = {
+	all_submissions: [
+		{
+			time: '1 month ago',
+			problem: 'Reverse a binary tree',
+			result: 'accepted',
+			runtime: '24ms',
+			memory: '290MB',
+			language: 'Python3',
+		},
+		{
+			time: '1 month ago',
+			problem: 'Reverse a binary tree',
+			result: 'accepted',
+			runtime: '24ms',
+			memory: '290MB',
+			language: 'Python3',
+		},
+		{
+			time: '1 month ago',
+			problem: 'Reverse a binary tree',
+			result: 'accepted',
+			runtime: '24ms',
+			memory: '290MB',
+			language: 'Python3',
+		},
+		{
+			time: '1 month ago',
+			problem: 'Reverse a binary tree',
+			result: 'accepted',
+			runtime: '24ms',
+			memory: '290MB',
+			language: 'Python3',
+		},
+		{
+			time: '1 month ago',
+			problem: 'Reverse a binary tree',
+			result: 'accepted',
+			runtime: '24ms',
+			memory: '290MB',
+			language: 'Python3',
+		},
+		{
+			time: '1 month ago',
+			problem: 'Reverse a binary tree',
+			result: 'accepted',
+			runtime: '24ms',
+			memory: '290MB',
+			language: 'Python3',
+		},
+		{
+			time: '1 month ago',
+			problem: 'Reverse a binary tree',
+			result: 'accepted',
+			runtime: '24ms',
+			memory: '290MB',
+			language: 'Python3',
+		},
+		{
+			time: '1 month ago',
+			problem: 'Reverse a binary tree',
+			result: 'accepted',
+			runtime: '24ms',
+			memory: '290MB',
+			language: 'Python3',
+		},
+		{
+			time: '1 month ago',
+			problem: 'Reverse a binary tree',
+			result: 'accepted',
+			runtime: '24ms',
+			memory: '290MB',
+			language: 'Python3',
+		},
+	],
+};
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: state,
+	mutations: {},
+	actions: {},
+	modules: {},
+});
